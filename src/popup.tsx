224f10cf9b9a043d5efca851620812a71f6e7553
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-06-10 11:47:04 +0100

    style: update popup container classes - Change flex classes to fc- prefix - Add temporary heading and spacing

--- src/popup.tsx
+++ src/popup.tsx
@@ -4,7 +4,11 @@ import "~style.css"
 
 function IndexPopup() {
   return (
-    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
+    <div className="plasmo-fc-flex plasmo-fc-items-center plasmo-fc-justify-center plasmo-fc-h-16 plasmo-fc-w-40">
+      <h1>Heyo</h1>
+      <br />
+      <br />
+      <br />
       <CountButton />
     </div>
   )
